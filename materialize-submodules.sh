a9e9532a5c12ff285e9d72d383b9907e04cefa48
Christian Decker <decker.christian@gmail.com>
2018-09-12 03:27:17 +0200

    fixup! ppa: Add script to materialize the submodules

--- materialize-submodules.sh
+++ materialize-submodules.sh
@@ -7,10 +7,19 @@ rm -rf .git/modules/external/jsmn .git/modules/external/libbase58 .git/modules/e
 git commit -am "scripted: Remove submodules for materialization"
 
 git clone --recursive https://github.com/zserge/jsmn external/jsmn
+(cd external/jsmn; git checkout 6784c826d9674915a4d89649c6288e6aecb4110d)
+
 git clone --recursive https://github.com/bitcoin/libbase58.git external/libbase58
+(cd external/libbase58; git checkout 16c2527608053d2cc2fa05b2e3b5ae96065d1410)
+
 git clone --recursive https://github.com/jedisct1/libsodium.git external/libsodium
+(cd external/libbacktrace; git checkout 675149b9b8b66ff44152553fb3ebf9858128363d)
+
 git clone --recursive https://github.com/ianlancetaylor/libbacktrace.git external/libbacktrace
+(cd external/libbacktrace; git clone 5a99ff7fed66b8ea8f09c9805c138524a7035ece)
+
 git clone --recursive https://github.com/ElementsProject/libwally-core.git external/libwally-core
+(cd external/libwally-core; git checkout d8e389e840df4ed0dbd68201852c8bccd38564d8)
 
 rm -rf external/jsmn/.git external/libbase58/.git/ external/libsodium/.git/ external/libbacktrace/.git/ external/libwally-core/.git
 
