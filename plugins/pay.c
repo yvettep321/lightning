a50858d80b228db8a6ea720b9ed3b6fc6ead6cdc
Christian Decker <cdecker@blockstream.com>
2022-01-08 13:22:23 +0100

    pay: Overhaul the way we generate UTC timestamps
    We have quite the complex UTC string construction, generating a
    format string using  to then pass it to ,
    somewhere along the line we added a crash that caused #4991, so
    here's my attempt at fixing that.
    
    Fixes: #4991

--- plugins/pay.c
+++ plugins/pay.c
@@ -1382,15 +1382,22 @@ static struct command_result *json_pay(struct command *cmd,
 }
 
 /* FIXME: Add this to ccan/time? */
-#define UTC_TIMELEN (sizeof("YYYY-mm-ddTHH:MM:SS.nnnZ"))
+/* The buffer size we need to serialize a UTC formatted string:
+ * `strlen("YYYY-mm-ddTHH:MM:SS.nnnZ") == 24 + 1` */
+#define UTC_TIMELEN 25
 static void utc_timestring(const struct timeabs *time, char str[UTC_TIMELEN])
 {
-	char iso8601_msec_fmt[sizeof("YYYY-mm-ddTHH:MM:SS.%03dZ")];
-
-	strftime(iso8601_msec_fmt, sizeof(iso8601_msec_fmt), "%FT%T.%%03dZ",
+	/* No need to be stingy with extra buffer space, so use what
+	 * we know is sufficient overall. */
+	char iso8601_fmt[UTC_TIMELEN];
+	/* Start by formatting the date and time */
+	strftime(iso8601_fmt, UTC_TIMELEN, "%Y-%m-%dT%H:%M:%S",
 		 gmtime(&time->ts.tv_sec));
-	snprintf(str, UTC_TIMELEN, iso8601_msec_fmt,
-		 (int) time->ts.tv_nsec / 1000000);
+	/* Now complete it by adding the milliseconds and the Zulu
+	 * identifier */
+	size_t len = snprintf(str, UTC_TIMELEN, "%19s.%03dZ", iso8601_fmt,
+			      (int)time->ts.tv_nsec / 1000000);
+	assert(len == UTC_TIMELEN - 1);
 }
 
 static void add_attempt(struct json_stream *ret,
