de8ff3b4ff1a6059512a7eb19e856b5198780e83
Christian Decker <decker.christian@gmail.com>
2022-01-18 10:44:20 +0100

    pytest: Add a test for the grpc plugin
    Currently still unencrypted, but will get its mTLS authentication in
    the next commits.

--- tests/test_cln_rs.py
+++ tests/test_cln_rs.py
@@ -4,6 +4,13 @@ from pyln.testing.utils import env, TEST_NETWORK
 import subprocess
 import pytest
 
+import grpc
+import node_pb2 as nodepb
+import pytest
+from fixtures import TEST_NETWORK
+from fixtures import *  # noqa: F401,F403
+from node_pb2_grpc import NodeStub
+from pyln.testing.utils import env
 
 # Skip the entire module if we don't have Rust. The same is true for
 # VALGRIND, since it sometimes causes false positives in
@@ -36,6 +43,10 @@ def test_plugin_start(node_factory):
     """
     bin_path = Path.cwd() / "target" / "debug" / "examples" / "cln-plugin-startup"
     l1 = node_factory.get_node(options={"plugin": str(bin_path), 'test-option': 31337})
+
+    # The plugin should be in the list of active plugins
+    plugins = l1.rpc.plugin('list')['plugins']
+    assert len([p for p in plugins if 'cln-plugin-startup' in p['name'] and p['active']]) == 1
 
     cfg = l1.rpc.listconfigs()
     p = cfg['plugins'][0]
